9d34f0ffd1ed325a68f16594dd5c71dc5de213c4
Manuel Alva Barrera <jalvabarrera1@nait.ca>
2024-07-30 13:37:54 -0600

    Completed Nav and Footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const toggleMenu = document.querySelector(".toggle-btn");
 const mainMenu = document.querySelector(".nav-bar");
+const dropDownMenus = document.querySelectorAll(".dropdown");
+const menuLinks = document.querySelectorAll(".menu-link");
 
+/**
+ * display the nav menu when clicking the toggle button in mobile view
+ */
 function displayMainMenu() {
   toggleMenu.addEventListener("click", (e) => {
     e.preventDefault();
@@ -9,3 +14,17 @@ function displayMainMenu() {
 }
 
 displayMainMenu();
+
+/**
+ * Display the dropdown menus when click on the menu links
+ */
+function displayDropDown() {
+  menuLinks.forEach((link, index) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      dropDownMenus[index].classList.toggle("active");
+    });
+  });
+}
+
+displayDropDown();
